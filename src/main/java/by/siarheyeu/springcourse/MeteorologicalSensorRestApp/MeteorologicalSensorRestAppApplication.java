81d8c5cec6af5dac05f44768316287a84896cb87
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-03-26 13:31:13 +0300

    Initial commit

---
+++ src/main/java/by/siarheyeu/springcourse/MeteorologicalSensorRestApp/MeteorologicalSensorRestAppApplication.java
@@ -0,0 +1,13 @@
+package by.siarheyeu.springcourse.MeteorologicalSensorRestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteorologicalSensorRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteorologicalSensorRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/siarheyeu/springcourse/MeteorologicalSensorRestApp/MeteorologicalSensorRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.siarheyeu.springcourse.MeteorologicalSensorRestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteorologicalSensorRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
