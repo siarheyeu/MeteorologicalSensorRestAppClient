be6e7a4dad470a3cf1500b1cbce2a40cb8eb347f
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-03-30 20:14:57 +0300

    SensorValidator

--- src/main/java/by/siarheyeu/springcourse/MeteorologicalSensorRestApp/dto/SensorDTO.java
+++ src/main/java/by/siarheyeu/springcourse/MeteorologicalSensorRestApp/dto/SensorDTO.java
@@ -1,4 +1,18 @@
 package by.siarheyeu.springcourse.MeteorologicalSensorRestApp.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class SensorDTO {
+    @NotEmpty(message = "Назвние не должно быть пустым!")
+    @Size(min = 3, max = 30, message = "Название сенсора должно быть от 3 до 30 символов!")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
