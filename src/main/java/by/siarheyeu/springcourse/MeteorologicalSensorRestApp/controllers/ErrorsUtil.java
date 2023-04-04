1a37822d1a63338ccb0347772db9a9ad768d9140
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-04-04 23:05:40 +0300

    ErrorsUtil

---
+++ src/main/java/by/siarheyeu/springcourse/MeteorologicalSensorRestApp/controllers/ErrorsUtil.java
@@ -0,0 +1,22 @@
+package by.siarheyeu.springcourse.MeteorologicalSensorRestApp.controllers;
+
+import by.siarheyeu.springcourse.MeteorologicalSensorRestApp.util.MeasurementException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+public class ErrorsUtil {
+    public static void returnErrorsToClient(BindingResult bindingResult) {
+        StringBuilder errorMsg = new StringBuilder();
+
+        List<FieldError> errors = bindingResult.getFieldErrors();
+        for (FieldError error : errors){
+            errorMsg.append(error.getField())
+                    .append(" - ").append(error.getDefaultMessage() == null ? error.getCode() : error.getDefaultMessage())
+                    .append(";");
+        }
+
+        throw new MeasurementException(errorMsg.toString());
+    }
+}
---
+++ src/main/java/by/siarheyeu/springcourse/MeteorologicalSensorRestApp/util/MeasurementException.java
@@ -0,0 +1,5 @@
+package by.siarheyeu.springcourse.MeteorologicalSensorRestApp.util;
+
+public class MeasurementException extends RuntimeException {
+    public MeasurementException(String s) { super(msg); }
+}
