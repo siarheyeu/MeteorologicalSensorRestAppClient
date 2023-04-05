8c65e6eb0708f21e5ab03c4316e07b81a03939a5
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-04-05 23:58:54 +0300

    MeasurementController

---
+++ src/main/java/by/siarheyeu/springcourse/MeteorologicalSensorRestApp/controllers/MeasurementsController.java
@@ -0,0 +1,42 @@
+package by.siarheyeu.springcourse.MeteorologicalSensorRestApp.controllers;
+
+
+import by.siarheyeu.springcourse.MeteorologicalSensorRestApp.models.Measurement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/measurements")
+public class MeasurementsController {
+
+    private final MeasurementService measurementService;
+    private final MeasurementValidator measurementValidator;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+
+    public MeasurementsController(MeasurementService measurementService,
+                                  MeasurementValidator measurementValidator,
+                                  ModelMapper modelMapper) {
+        this.measurementService = measurementService;
+        this.measurementValidator = measurementValidator;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<HttpStatus> add(@RequestBody @Valid MeasurementDTO measurementDTO,
+                                          BindingResult bindingResult){
+        Measurement measurement  = convertToMeasurement(measurementDTO);
+
+        measurementValidator.validate(measurementToAdd, bindingResult);
+
+    }
+}
