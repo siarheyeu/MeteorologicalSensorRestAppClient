decc8951285ee6c1915ad03f751e239ebabcd3a5
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-04-17 23:57:15 +0300

    Client changes

--- src/main/java/by/siarheyeu/springcourse/MeteorologicalSensorRestApp/Client.java
+++ src/main/java/by/siarheyeu/springcourse/MeteorologicalSensorRestApp/Client.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 
@@ -21,8 +23,28 @@ public class Client {
 		double maxTemperature = 45.0;
 		for (int i = 0; i < 500; i++) {
 			System.out.println(i);
-
+			sendMeasurement (minTemperature + (random.nextDouble() * (maxTemperature - minTemperature)),
+				random.nextBoolean(), sensorName);
 		}
+	}
 
+	private static void registerSensor(String sensorName){
+		final String url = "http://localhost:8080/sensor/registration";
+
+		Map<String, Object> jsonData = new HashMap<>();
+		jsonData.put("name", sensorName);
+
+		makePostRequestWithJSON(url, jsonData);
+	}
+
+	private static void sendMeasurement(double value, boolean raining, String sensorName) {
+		final String url = "http://localhost:8080/measurement/add";
+
+		Map<String, Object> jsonData = new HashMap<>();
+		jsonData.put("value", value);
+		jsonData.put("raining", raining);
+		jsonData.put("sensor", Map.of("name", sensorName));
+
+		makePostRequestWithJSONData(url, jsonData);
 	}
 }
