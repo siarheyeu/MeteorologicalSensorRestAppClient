4500d049f7623f179ce809f177bd1da60e08977f
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-04-21 22:59:59 +0300

    DrawChart

---
+++ src/main/java/by/siarheyeu/springcourse/MeteorologicalSensorRestApp/DrawChart.java
@@ -0,0 +1,19 @@
+package by.siarheyeu.springcourse.MeteorologicalSensorRestApp;
+
+
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+public class DrawChart {
+    public static void main(String[] args) {
+        List<Double> temperatures = getTemperaturesFromServer();
+        drawChart(temperatures);
+        }
+
+        private static List<Double> getTemperaturesFromServer(){
+        final RestTemplate restTemplate = new RestTemplate();
+        final String url = "http://localhost:8080/measurements";
+        }
+
+}
