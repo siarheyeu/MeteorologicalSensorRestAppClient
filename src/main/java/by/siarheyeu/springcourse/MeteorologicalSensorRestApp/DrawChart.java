224f6d0501c187b9c00f73a61bec9378f44e0ccf
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-04-23 23:39:28 +0300

    DrawChart change 2

--- src/main/java/by/siarheyeu/springcourse/MeteorologicalSensorRestApp/DrawChart.java
+++ src/main/java/by/siarheyeu/springcourse/MeteorologicalSensorRestApp/DrawChart.java
@@ -1,10 +1,14 @@
 package by.siarheyeu.springcourse.MeteorologicalSensorRestApp;
 
 
+import by.siarheyeu.springcourse.MeteorologicalSensorRestApp.dto.MeasurementDTO;
 import by.siarheyeu.springcourse.MeteorologicalSensorRestApp.dto.MeasurementsResponse;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DrawChart {
     public static void main(String[] args) {
@@ -17,5 +21,20 @@ public class DrawChart {
         final String url = "http://localhost:8080/measurements";
         }
 
-        MeasurementsResponse
+        MeasurementsResponse jsonResponse = restTemplate.getForObject (url, MeasurementsResponse.class);
+
+        if (jsonResponse == null || jsonResponse.getMeasurements() == null)
+            return Collections.emptyList();
+
+        return jsonResponse.getMeasurements().stream().map(MeasurementDTO::getValue)
+            .collect(Collectors.toList());
+
+        private static void drawChart(List<Double> temperatures){
+            double[] xData = IntStream.range(0, temperatures.size()).asDoubleStream().toArray();
+            double[] yData = temperatures.stream().mapToDouble(x -> x).toArray();
+
+            XYChart chart = QuickChart.getChart("Temperatures", "X", "Y", "temperature", xData, yData);
+
+            new SwingWrapper(chart).displayChart();
+        }
 }
